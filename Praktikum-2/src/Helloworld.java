377ca712af033bcd0af024888ad542de127ea43c
Raissa Ogya <raissaogya@gmail.com>
2026-09-08 08:45:49 +0700

    Struktur Dasar Program Java

--- Praktikum-2/src/Helloworld.java
+++ Praktikum-2/src/Helloworld.java
@@ -1,5 +1,12 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class Helloworld {
     public static void main(String[] args){
         System.out.println("Raissa Ogya");
+        System.out.println("Nama: ");
+        System.out.println("Budi");
+        System.out.println();
+        System.out.println("Ini baris baru");
     }
 }
+
