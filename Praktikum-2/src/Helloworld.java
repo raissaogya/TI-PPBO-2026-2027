dc1f679d7bff0ea909bb3b6c82c6c3bfd03fa3c3
Raissa Ogya <raissaogya@gmail.com>
2026-09-08 10:00:19 +0700

    B.2. Casting(Konversi Tipe Data)

--- Praktikum-2/src/Helloworld.java
+++ Praktikum-2/src/Helloworld.java
@@ -7,6 +7,14 @@ public class Helloworld {
         /* Baris dibawah ini
             mencetak NIM mahasiswa*/
         System.out.println("NIM:12345");
+
+        int nilaiBulat = 9;
+        double nilaiDouble = nilaiBulat; //widening otomatis
+        System.out.println("Widening: " + nilaiDouble);
+
+        double pecahan = 9.8;
+        int hasilCasting = (int) pecahan; //narrowing eksplisit
+        System.out.println("Narrowing: " + hasilCasting);
     }
 }
 
