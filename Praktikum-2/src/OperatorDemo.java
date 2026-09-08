de5a9d018e02ef49ea7f09b9d15763b1ea028384
Raissa Ogya <raissaogya@gmail.com>
2026-09-08 10:27:40 +0700

    B.3. Operator

--- Praktikum-2/src/OperatorDemo.java
+++ Praktikum-2/src/OperatorDemo.java
@@ -6,5 +6,11 @@ public class OperatorDemo {
         System.out.println("Kali: " + (a * b));
         System.out.println("Bagi: " + (a / b));
         System.out.println("Sisa Bagi: " + (a % b));
+
+        int nilaiUjian = 80;
+        boolean hadir = true;
+        System.out.println("Lulus (>=75): " + (nilaiUjian >= 75));
+        System.out.println("Lulus DAN hadir: " + (nilaiUjian >=75 && hadir));
+
     }
 }
