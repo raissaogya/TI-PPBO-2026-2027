e7c348aaa5e87d5297998b6760f44441ad8d9906
Raissa Ogya <raissaogya@gmail.com>
2026-09-08 11:05:15 +0700

    Latihan 2

--- Praktikum-2/src/BiodataSaya.java
+++ Praktikum-2/src/BiodataSaya.java
@@ -1,10 +1,11 @@
 public class BiodataSaya {
     public static void main(String[] args){
         //Menampilkan nama mahasiswa
-        System.out.println("Nama: Raissa Ogya");
+        System.out.print("Nama: Raissa Ogya - ");
+        System.out.print("2025573010063");
 
         //Menampilkan NIM dan program studi
-        System.out.println("NIM: 2025573010063");
+        System.out.println();
         System.out.println("Program Studi: Teknik Informatika");
     }
 }
